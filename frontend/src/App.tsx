801bd2fa4ae73468fc5cdf8590c2c55bc25e335d
Sachin Sen <sachinsen1920@gmail.com>
2025-09-16 18:39:59 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-  return (
-    <div className='text-amber-950'>App</div>
-  )
+  return <div className="text-amber-950">App</div>;
 }
 
-export default App
+export default App;
