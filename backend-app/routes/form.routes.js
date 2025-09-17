a639331c3cbd3092682a878d297ec4df3747ea67
Sachin Sen <sachinsen1920@gmail.com>
2025-09-17 13:15:16 +0530

    add

--- backend-app/routes/form.routes.js
+++ backend-app/routes/form.routes.js
@@ -6,7 +6,7 @@ import {
   submitForm,
   getAllResponses,
   deleteResponse,
-  sendFormInvites
+  sendFormInvites,
 } from "../controllers/form.controller.js";
 import { protect } from "../middlewares/AuthMiddleware.js";
 
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.post("/generate", generateFormLink);
 router.get("/:linkId", getFormStructure);
-router.post("/:linkId/submit",protect, submitForm);
+router.post("/:linkId/submit", protect, submitForm);
 router.get("/responses/all", getAllResponses);
 router.delete("/responses/:id", deleteResponse);
 router.post("/send-invites", sendFormInvites);
